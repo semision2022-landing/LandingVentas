74f5f0e91106cee636b5d1c7d093a2262b02dfb3
expresCocina <SALCRISTHI5411@GMAIL.COM>
2026-03-26 17:29:17 -0500

    fix: remove unused next/image imports to pass build

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+
 import { Phone, Mail, MapPin, Clock } from 'lucide-react'
 
 const footerLinks = {
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Image from 'next/image'
+
 import { useState, useEffect } from 'react'
 import { Menu, X } from 'lucide-react'
 
